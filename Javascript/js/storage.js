6cda15eb6d7ad43bb45b4f4154a0379b77650f68
Navi Talib <talibali786.ta@gmail.com>
2022-05-09 17:41:52 +0530

    learning storage in js done

--- Javascript/js/storage.js
+++ Javascript/js/storage.js
@@ -17,4 +17,19 @@ localStorage.setItem('Name','Navi');
 
 localStorage.removeItem('Name2');
 
+//localStorage.clear();  is used to clear everything
+
+
+//storing JSON objects
+
+let name2 = ['Navi','Talib'];
+localStorage.setItem('Namex',JSON.stringify(name2));
+
+
+//read JSON object
+let namex = JSON.parse(localStorage.getItem('Namex'));
+console.log(namex);
+
+
+
 
